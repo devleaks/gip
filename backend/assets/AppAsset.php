03b1508ad57f112cbc90d737aaf136d2d76de3ee
Pierre Mareschal <pierre.mareschal@oscars-sa.eu>
2017-12-04 17:40:56 +0100

    added css pattern for demo

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -20,6 +20,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+    	'css/patterns.css',
     ];
     public $js = [
 		'js/bootbox.min.js',
